b33bc5aee18b0d807714a54fbe0fe277de0fc64d
Cats_have7lives <165317902+CatsHave7lives@users.noreply.github.com>
2025-08-24 23:37:27 +0100

    done

--- lib_msh/msh_quit.c
+++ lib_msh/msh_quit.c
@@ -6,7 +6,7 @@
 /*   By: aessaber <aessaber@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/11 08:18:13 by aessaber          #+#    #+#             */
-/*   Updated: 2025/08/23 23:22:21 by aessaber         ###   ########.fr       */
+/*   Updated: 2025/08/24 23:30:48 by aessaber         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,8 +20,6 @@ static void	static_unlink(void *file_name)
 
 void	msh_quit(t_data *data, int status)
 {
-	if (data->heredoc_count > 16)
-		msh_puterr("msh: maximum here-document count exceeded\n");
 	if (!data)
 		exit(status);
 	if (data->heredoc_files)
--- msh_execution/msh_execute_cmd.c
+++ msh_execution/msh_execute_cmd.c
@@ -6,7 +6,7 @@
 /*   By: aessaber <aessaber@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/20 13:26:10 by aessaber          #+#    #+#             */
-/*   Updated: 2025/08/24 22:32:42 by aessaber         ###   ########.fr       */
+/*   Updated: 2025/08/24 23:34:01 by aessaber         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,7 +26,6 @@ int	msh_execute_cmd(t_data *data, int status, t_env **env, t_gc **gc)
 
 	if (data->abs->arg && data->abs->arg[0])
 	{
-		msh_handle_redir(data->abs->red_l, false);
 		if (static_is_builtin_parent(data->abs->arg[0]))
 		{
 			if (msh_handle_redir(data->abs->red_l, false) == EXIT_FAILURE)
